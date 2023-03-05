26d405dc723f85596f3882cdcd64fc19dbcaffea
wcAlex <alex.chi.wang@gmail.com>
2023-03-05 15:53:40 -0800

    p733

---
+++ python/blind75/floodFill/solution.py
@@ -0,0 +1,43 @@
+class Solution:
+    def floodFill(self, image: list[list[int]], sr: int, sc: int, color: int) -> list[list[int]]:
+        def flood(image: list[list[int]], sr: int, sc: int, new_color: int, old_color):
+            r_cnt, c_cnt = len(image), len(image[0])
+
+            if sr < 0 or sr >= r_cnt or sc < 0 or sc >= c_cnt or image[sr][sc] != old_color:
+                return 
+            else:
+                image[sr][sc] = new_color
+                flood(image, sr+1, sc, new_color, old_color)
+                flood(image, sr, sc+1, new_color, old_color)
+                flood(image, sr-1, sc, new_color, old_color)
+                flood(image, sr, sc-1, new_color, old_color)
+
+        old_color = image[sr][sc]
+        flood(image, sr, sc, color, old_color)
+        return image
+        
+    def floodFill2(self, image: list[list[int]], sr: int, sc: int, color: int) -> list[list[int]]:
+        old_color = image[sr][sc]
+        new_color = color
+
+        def flood(image: list[list[int]], sr: int, sc: int):
+            r_cnt, c_cnt = len(image), len(image[0])
+
+            if sr < 0 or sr >= r_cnt or sc < 0 or sc >= c_cnt or image[sr][sc] == new_color:
+                return 
+
+            if image[sr][sc] == old_color:
+                image[sr][sc] = new_color
+                flood(image, sr+1, sc)
+                flood(image, sr, sc+1)
+                flood(image, sr-1, sc)
+                flood(image, sr, sc-1)
+
+        flood(image, sr, sc)
+        return image
+        
+
+        
+
+    
+    
---
+++ python/blind75/floodFill/test.py
@@ -0,0 +1,14 @@
+from solution import Solution
+import unittest
+
+class TestSum(unittest.TestCase):
+
+    def test_flood_case1(self):
+        sol = Solution()
+
+        res = sol.floodFill(image = [[1,1,1],[1,1,0],[1,0,1]], sr = 1, sc = 1, color = 2)
+        print(res)
+        self.assertEqual(res, [[2,2,2],[2,2,0],[2,0,1]])
+
+if __name__ == '__main__':
+    unittest.main()
